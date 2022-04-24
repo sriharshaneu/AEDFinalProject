fc562432875f506c807f2754b6047a719247c0b3
Sreyas Borra <borra.s@northeastern.edu>
2022-04-24 08:27:00 -0400

    added delivery.java

---
+++ src/starting/Delivery.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package starting;
+
+/**
+ *
+ * @author sriha
+ */
+public class Delivery {
+
+	private int delivery_order_id;
+	private int order_id;
+	private int status;
+
+	public int getDelivery_order_id() {
+		return delivery_order_id;
+	}
+
+	public void setDelivery_order_id(int delivery_order_id) {
+		this.delivery_order_id = delivery_order_id;
+	}
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+}
