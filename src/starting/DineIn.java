6024d2badece2ede3366fd8eecad862ca556f842
Sreyas Borra <borra.s@northeastern.edu>
2022-04-24 17:39:00 -0400

    added dinein.java

---
+++ src/starting/DineIn.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package starting;
+
+/**
+ *
+ * @author sriha
+ */
+public class DineIn {
+	private int dinein_order_id;
+	private int order_id;
+	private int status;
+
+	
+
+	public int getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public int getDinein_order_id() {
+		return dinein_order_id;
+	}
+
+	public void setDinein_order_id(int dinein_order_id) {
+		this.dinein_order_id = dinein_order_id;
+	}
+}
