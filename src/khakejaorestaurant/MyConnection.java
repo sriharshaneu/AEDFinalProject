9a550c536bc97ea9100cb0976c27217be26d5060
Prathik Reddy Arasu <arasu.p@northeastern.edu>
2022-05-01 10:51:00 -0400

    added dbconnection

---
+++ src/khakejaorestaurant/MyConnection.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package khakejaorestaurant;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sriha
+ */
+public class MyConnection
+{
+
+    public static Connection getConnection()
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/menu_item?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "root@123");
+        } catch (SQLException | ClassNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+
+        }
+
+        return con;
+    }
+
+    static Object getconnection()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
